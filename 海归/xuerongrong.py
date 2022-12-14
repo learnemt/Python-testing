5543c154a12bcc9128c864c4bba880ca2b4a74bf
lty <77092327+learnemt@users.noreply.github.com>
2022-12-14 17:32:20 +0800

    新增海归

---
+++ 海归/xuerongrong.py
@@ -0,0 +1,27 @@
+import turtle as hg
+
+#hg.mode('logo')
+hg.setup(800,600)
+hg.title('雪容融')
+# hg.pensize(6)
+# hg.pencolor('#f2dc95')
+hg.speed(10)
+
+
+#头饰
+
+
+# 头
+hg.circle(100,360)
+hg.penup()
+hg.goto(-60,100)
+hg.pendown()
+hg.setheading(90)
+for i in range(2):
+    print(i)
+    hg.circle(-26,180)
+    hg.circle(19,180)
+# 脸
+
+
+hg.done()
